bad5d3436ebff24a72b81fccf0b9f4f61f5fe37c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2022-08-25 11:12:05 +0200

    Locate gfortran on Mac's

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use cmake;
+use std::env;
 
 fn main() {
     let dst = cmake::build("amos");
@@ -7,6 +8,11 @@ fn main() {
         "cargo:rustc-link-search=native={}",
         dst.join("amos").display()
     );
+
+    if env::consts::OS.eq("macos") {
+        // Hack to locate gfortran on Mac with gcc installed via Homebrew
+        println!("cargo:rustc-link-search=/usr/local/lib/gcc/current/");
+    }
 
     println!("cargo:rustc-link-lib=static=amos");
     // Link using gfortran instead of gcc, note this must come after the above command
